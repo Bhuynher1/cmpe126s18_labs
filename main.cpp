e6b7b007bb755e610d7a5716957b698f77069901
blakehuynh <blake.huynh@sjsu.edu>
2021-02-05 00:11:10 -0800

    2nd commit changing values to understand the calculations

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello Thursday everyone!";
     std::cout << "\nfirst commit!";
+    std::cout <, "\n 2nd commit with changes to understand calculations";
     return 0;
 }
