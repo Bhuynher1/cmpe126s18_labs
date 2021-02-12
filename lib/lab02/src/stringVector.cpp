2b9d04f9aed658265e0148c9de03f390463d9c03
blakehuynh <blake.huynh@sjsu.edu>
2021-02-12 10:58:41 -0800

    3rd commit lab 2 start

--- lib/lab02/src/stringVector.cpp
+++ lib/lab02/src/stringVector.cpp
@@ -2,31 +2,44 @@
 
 namespace lab2 {
     stringVector::stringVector() {
-
+        this->data = nullptr;
+        length = 0;
+        allocated_length = 0;
     }
 
     stringVector::~stringVector() {
+        delete[] data;
+        data = nullptr;
+
 
     }
 
     unsigned stringVector::size() const{
-        //return ;
+        return length;
     }
 
     unsigned stringVector::capacity() const{
-        //return ;
+        return allocated_length;
     }
 
     void stringVector::reserve(unsigned new_size) {
 
+        unsigned array1[new_size];
+    //create new array that fills up to given size and doesnt take more than the given allocated size
     }
 
     bool stringVector::empty() const{
-        //return ;
+        if (array1 == )
     }
 
     void stringVector::append(std::string new_data) {
+//case 1: array is empty;
+//size: 5 capacity: 6;
+//size: 6 capacity: 10;
 
+//make new array with doubled capacity;
+//copy data over to new array;
+//change the pointer to point to the new array;
     }
 
     void stringVector::swap(unsigned pos1, unsigned pos2) {
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello Thursday everyone!";
     std::cout << "\nfirst commit!";
-    std::cout <, "\n 2nd commit with changes to understand calculations";
+    std::cout << "\n 2nd commit with changes to understand calculations";
+    std::cout << "\n3rd commit where we started working on lab 2";
     return 0;
 }
