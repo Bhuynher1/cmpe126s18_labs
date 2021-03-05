2b71e60893dcbfc49e847bd05d1ee1af03982586
blakehuynh <blake.huynh@sjsu.edu>
2021-03-04 20:08:49 -0800

    lab04 start with some functions being defined.

--- lib/lab04/src/calculator.cpp
+++ lib/lab04/src/calculator.cpp
@@ -2,9 +2,58 @@
 
 #include <string>
 #include "calculator.h"
-namespace lab4 {
-    void calculator::parse_to_infix(std::string &input_expression) {
+#include <sstream>
 
+namespace lab4 {
+    bool is_number(std::string input_string);
+
+    bool is_operator(std::string input_string);
+
+    int get_number(std::string input_string);
+
+    std::string get_operator(std::string input_string);
+
+    int operator_priority(std::string operator_in);
+
+    void calculator::parse_to_infix(std::string &input_expression) {
+        std::string temp = "";
+        std::string num = "";
+
+        for (int i = 0; i < input_expression.length(); i++) {
+            std::string string1 = input_expression.substr(i,1);
+
+            if (is_number(string1)) {
+                num += string1;
+            }
+            else if (is_operator(string1)) {
+                if (num.length() > 0) {
+                    temp += num + "";
+                    num = "";
+                }
+                temp += string1;
+
+                if (i < input_expression.length() -1)
+                    temp += "";
+            }
+            else if (string1.compare("") == 0) {
+                if (num.length() > 0) {
+                    temp += num + "";
+                    num = "";
+                }
+            }
+        }
+        if (num.length() > 0) {
+            temp += num;
+            num = "";
+        }
+        std::istringstream ss(temp);
+        std::string string2;
+
+        while (std::getline(ss, string2, ' '))
+        {
+            infix_expression.enqueue(string2);
+        }
+        input_expression = temp;
     }
 
     void calculator::convert_to_postfix(lab3::fifo infix_expression) {
@@ -16,7 +65,7 @@ namespace lab4 {
     }
 
     calculator::calculator(std::string &input_expression) {
-
+        parse_to_infix(input_expression);
     }
 
     std::istream &operator>>(std::istream &stream, calculator &RHS) {
@@ -28,18 +77,28 @@ namespace lab4 {
     }
 
     std::ostream &operator<<(std::ostream &stream, calculator &RHS) {
-        return stream;
+
     }
 
 
     // AUXILIARY FUNCTIONS
-    bool is_number(std::string input_string);
+    bool is_number(std::string input_string) {
 
-    bool is_operator(std::string input_string);
+    }
 
-    int get_number(std::string input_string);
+    bool is_operator(std::string input_string) {
 
-    std::string get_operator(std::string input_string);
+    }
 
-    int operator_priority(std::string operator_in);
+    int get_number(std::string input_string) {
+
+    }
+
+    std::string get_operator(std::string input_string) {
+
+    }
+
+    int operator_priority(std::string operator_in) {
+
+    }
 }
