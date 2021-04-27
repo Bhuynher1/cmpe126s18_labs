9f3e07a349feea6f7439124bb854cb232e431be0
Yu Li <yu.li01@sjsu.edu>
2021-04-27 14:43:26 -0700

    rev typo

--- lib/lab08/src/sorts.cpp
+++ lib/lab08/src/sorts.cpp
@@ -2,16 +2,16 @@
 
 namespace lab6{
     // Auxiliary Functions
-    unsigned partition(int* to_sort, int low, int high);
+    unsigned partition(doubly_linked_list& to_sort, int low, int high);
 
     doubly_linked_list recursive_merge_sort(doubly_linked_list to_sort);
     doubly_linked_list merge(doubly_linked_list left, doubly_linked_list right);
 
-    void quicksort(int* to_sort, int low, int high){
+    void quicksort(doubly_linked_list& to_sort, int low, int high){
 
     }
 
-    unsigned partititon(int* to_sort, int low, int high){
+    unsigned partition(doubly_linked_list& to_sort, int low, int high){
 
     }
 
